77690d28974d838371a47c1e912d1f4e2dbe48c9
Alexander <Sakeran@gmail.com>
2022-08-26 15:30:45 -0400

    Update toggle button behavior

--- src/components/About/Clients/TeammateBio/ToggleButton.svelte
+++ src/components/About/Clients/TeammateBio/ToggleButton.svelte
@@ -11,35 +11,30 @@
 
   export let data: TeammateBioData;
   export let expanded: boolean;
-
-  let showButton = false;
-
-  onMount(() => showButton = true);
 </script>
 
-{#if showButton}
-  <button
-    on:click
-    aria-expanded={expanded}
-    class="mx-auto w-14 aspect-square rounded-[50%] bg-red-coral hocus:bg-blue-rapture motion-safe:transition-colors motion-safe:ease-in-out grid place-items-center"
+<button
+  on:click
+  aria-expanded={expanded}
+  class:bg-blue-rapture={expanded}
+  class="mx-auto w-14 aspect-square rounded-[50%] bg-red-coral hocus:bg-blue-rapture motion-safe:transition-colors motion-safe:ease-in-out grid place-items-center"
+>
+  <svg
+    class="motion-safe:transition-transform motion-safe:ease-in-out"
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    aria-hidden="true"
+    ><path
+      fill="#012F34"
+      fill-rule="evenodd"
+      d="M10 0v5.999L16 6v4h-6v6H6v-6H0V6h6V0h4z"
+    /></svg
   >
-    <svg
-      class="motion-safe:transition-transform motion-safe:ease-in-out"
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      aria-hidden="true"
-      ><path
-        fill="#012F34"
-        fill-rule="evenodd"
-        d="M10 0v5.999L16 6v4h-6v6H6v-6H0V6h6V0h4z"
-      /></svg
-    >
-    <span class="sr-only"
-      >{expanded ? "Hide" : "Show more"} info for {data.name}</span
-    ></button
-  >
-{/if}
+  <span class="sr-only"
+    >{expanded ? "Hide" : "Show more"} info for {data.name}</span
+  ></button
+>
 
 <style lang="postcss">
   [aria-expanded="true"] > svg {

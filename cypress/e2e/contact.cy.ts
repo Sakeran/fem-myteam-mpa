a4d26b1caea6d6668828ba88b119b60efe137b8d
Alexander <Sakeran@gmail.com>
2022-08-28 14:44:17 -0400

    Implement Contact page

--- cypress/e2e/contact.cy.ts
+++ cypress/e2e/contact.cy.ts
@@ -1,5 +1,13 @@
-describe("Text Inputs", () => {
-  it("works", () => {
-    expect(true).to.eql(true);
+describe("Contact Page", () => {
+  it("Can send the contact form", () => {
+    cy.visit("/contact");
+
+    cy.get('[name="name"]').type("A Person");
+    cy.get('[name="email"]').type("me@example.com");
+    cy.get('[name="company-name"]').type("A Company");
+    cy.get('[name="title"]').type("Some Message");
+    cy.get('[name="message"]').type("Hello,{enter}This is a message.");
+
+    cy.contains("submit").click();
   });
 });

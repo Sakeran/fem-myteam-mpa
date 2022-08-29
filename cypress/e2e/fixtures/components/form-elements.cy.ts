27328b8e7fe83b3a36a667276f42261141f13295
Alexander <Sakeran@gmail.com>
2022-08-29 11:58:33 -0400

    Improve form-element tests

--- cypress/e2e/fixtures/components/form-elements.cy.ts
+++ cypress/e2e/fixtures/components/form-elements.cy.ts
@@ -6,14 +6,14 @@ describe("Form Elements", () => {
   it("Can correctly enter text", () => {
     cy.get('[type="text"]').as("text-input").type("My Name");
 
-    cy.get("@text-input").parent().get("p").should("not.be.visible");
+    cy.get("@text-input").next('p').should("not.be.visible");
   });
 
   it("Can show an error on empty text input blur", () => {
-    cy.get('[type="text"]').as("text-input").focus().blur();
+    cy.get('[type="text"]').as("text-input").click().blur();
 
     cy.get("@text-input")
-      .parent()
+      .next()
       .contains("This field is required")
       .should("be.visible");
   });
@@ -21,14 +21,14 @@ describe("Form Elements", () => {
   it("Can correctly enter an email", () => {
     cy.get('[type="email"]').as("email-input").type("hi@example.com");
 
-    cy.get("@email-input").parent().get("p").should("not.be.visible");
+    cy.get("@email-input").next().should("not.be.visible");
   });
 
   it("Can show an error on empty email input blur", () => {
-    cy.get('[type="email"]').as("email-input").focus().blur();
+    cy.get('[type="email"]').as("email-input").click().blur();
 
     cy.get("@email-input")
-      .parent()
+      .next()
       .contains("This field is required")
       .should("be.visible");
   });
@@ -37,7 +37,7 @@ describe("Form Elements", () => {
     cy.get('[type="email"]').as("email-input").type("invalidemail");
 
     cy.get("@email-input")
-      .parent()
+      .next()
       .contains("Enter a valid email address")
       .should("be.visible");
   });
@@ -45,14 +45,14 @@ describe("Form Elements", () => {
   it("Can correctly enter into a text area", () => {
     cy.get("textarea").as("text-area").type("Hello,{enter}This is a message.");
 
-    cy.get("@text-area").parent().get("p").should("not.be.visible");
+    cy.get("@text-area").next('p').should("not.be.visible");
   });
 
   it("Can show an error on empty textarea blur", () => {
-    cy.get("textarea").as("text-area").focus().blur();
+    cy.get("textarea").as("text-area").click().blur();
 
     cy.get("@text-area")
-      .parent()
+      .next()
       .contains("Please enter a message")
       .should("be.visible");
   });
